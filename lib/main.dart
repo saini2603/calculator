6ae81e388a11f7eae5ad8c6236ed01bfb61a7499
Rohit Saini <rohitsaini8872@gmail.com>
2022-11-17 00:23:12 -0600

    Lab4 is updated

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,15 @@ class _calculatorAppState extends State<calculatorApp> {
   //Button Widget
   Widget calcbutton(String buttontext,Color buttoncolor,Color textcolor){
     return  Container(
+      width: 98,
+      height: 100,
+      decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black12,
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(5)
+      ),
       child: MaterialButton(
         onPressed: (){
           calculation(buttontext);
@@ -36,7 +45,36 @@ class _calculatorAppState extends State<calculatorApp> {
             color: textcolor,
           ),
         ),
-        shape: CircleBorder(),
+        // shape: CircleBorder(),
+        color: buttoncolor,
+        padding: EdgeInsets.all(20),
+
+      ),
+    );
+  }
+  Widget calcbutton1(String buttontext,Color buttoncolor,Color textcolor){
+    return  Container(
+      width: 196,
+      height: 100,
+
+      decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.black12,
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(5)
+      ),
+      child: MaterialButton(
+
+        onPressed: (){
+          calculation(buttontext);
+        },
+        child:  Text('$buttontext',
+          style: TextStyle(
+            fontSize: 35,
+            color: textcolor,
+          ),
+        ),
         color: buttoncolor,
         padding: EdgeInsets.all(20),
       ),
@@ -45,11 +83,9 @@ class _calculatorAppState extends State<calculatorApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(title: Text('Calculator'),backgroundColor: Colors.teal,),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 9),
-        child: Column(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(title: Text('Calculator'),backgroundColor: Colors.blue,),
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             SingleChildScrollView(
@@ -59,96 +95,64 @@ class _calculatorAppState extends State<calculatorApp> {
                 children: [
                   Padding(padding: EdgeInsets.all(10),
                   child: Text('$text',textAlign: TextAlign.right,
-                  style: TextStyle(color:Colors.teal,
-                  fontSize: 100
+                  style: TextStyle(color:Colors.white,
+                  fontSize: 100,
                   ),
                   ),
                   )
                 ],
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('AC',Colors.teal,Colors.white),
-                calcbutton('+/-',Colors.white,Colors.teal),
-                calcbutton('%',Colors.white,Colors.teal),
-                calcbutton('/',Colors.white,Colors.teal),
-              ],
-            ),
-            SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('7',Colors.white,Colors.black),
-                calcbutton('8',Colors.white,Colors.black),
-                calcbutton('9',Colors.white,Colors.black),
-                calcbutton('x',Colors.white,Colors.teal),
-              ],
-            ),
-            SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('4',Colors.white,Colors.black),
-                calcbutton('5',Colors.white,Colors.black),
-                calcbutton('6',Colors.white,Colors.black),
-                calcbutton('-',Colors.white,Colors.teal),
-              ],
-            ),
-            SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                calcbutton('1',Colors.white,Colors.black),
-                calcbutton('2',Colors.white,Colors.black),
-                calcbutton('3',Colors.white,Colors.black),
-                calcbutton('+',Colors.white,Colors.teal),
-              ],
-            ),
-            SizedBox(height: 10,),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
 
-                MaterialButton(
-                  padding: EdgeInsets.all(10),
-                  onPressed: (){
-                    calculation('00');
-                  },
-                  shape: StadiumBorder(),
-                  child: Text('00',
-                    style: TextStyle(
-                        fontSize: 35,
-                        color: Colors.black),
-                  ),
-                  color: Colors.white,
-                ),
-                MaterialButton(
-                  padding: EdgeInsets.all(10),
-                  onPressed: (){
-                    calculation('0');
-                  },
-                  shape: StadiumBorder(),
-                  child: Text('0',
-                    style: TextStyle(
-                        fontSize: 35,
-                        color: Colors.black),
-                  ),
-                  color: Colors.white,
-                ),
-                calcbutton('.',Colors.white,Colors.black),
-                calcbutton('=',Colors.teal,Colors.white),
+            Row(
+              children: <Widget>[
+                calcbutton('AC',Colors.blueGrey,Colors.white),
+                calcbutton('+/-',Colors.blueGrey,Colors.white),
+                calcbutton('%',Colors.blueGrey,Colors.white),
+                calcbutton('÷',Colors.orange,Colors.white),
               ],
             ),
-            SizedBox(height: 10,),
+            //SizedBox(height: 10,),
+            Row(
+              children: <Widget>[
+                calcbutton('7',Colors.blue,Colors.white),
+                calcbutton('8',Colors.blue,Colors.white),
+                calcbutton('9',Colors.blue,Colors.white),
+                calcbutton('x',Colors.orange,Colors.white),
+              ],
+            ),
+            //SizedBox(height: 10,),
+            Row(
+              children: <Widget>[
+                calcbutton('4',Colors.blue,Colors.white),
+                calcbutton('5',Colors.blue,Colors.white),
+                calcbutton('6',Colors.blue,Colors.white),
+                calcbutton('-',Colors.orange,Colors.white),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                calcbutton('1',Colors.blue,Colors.white),
+                calcbutton('2',Colors.blue,Colors.white),
+                calcbutton('3',Colors.blue,Colors.white),
+                calcbutton('+',Colors.orange,Colors.white),
+              ],
+            ),
+            //SizedBox(height: 10,),
+            Row(
+              children: <Widget>[
+                calcbutton1('0',Colors.blue,Colors.white),
+                calcbutton('.',Colors.blue,Colors.white),
+                calcbutton('=',Colors.orange,Colors.white),
+              ],
+            ),
+            //SizedBox(height: 10,),
           ],
         ),
-      ),
-    );
+      );
   }
 
-  //Calculator logic
+  //calculation_code
   dynamic text ='0';
   double num1 = 0;
   double num2 = 0;
@@ -173,11 +177,11 @@ class _calculatorAppState extends State<calculatorApp> {
         finalResult = sub();
       } else if (preOpr == 'x') {
         finalResult = mul();
-      } else if (preOpr == '/') {
+      } else if (preOpr == '÷') {
         finalResult = div();
       }
     } else if (buttonText == '+' || buttonText == '-' || buttonText == 'x' ||
-        buttonText == '/' || buttonText == '=') {
+        buttonText == '÷' || buttonText == '=') {
       if (num1 == 0) {
         num1 = double.parse(result);
       } else {
@@ -190,7 +194,7 @@ class _calculatorAppState extends State<calculatorApp> {
         finalResult = sub();
       } else if (opr == 'x') {
         finalResult = mul();
-      } else if (opr == '/') {
+      } else if (opr == '÷') {
         finalResult = div();
       }
       preOpr = opr;
